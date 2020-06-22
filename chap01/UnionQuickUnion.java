17d69b4b0d9850673e590b845fbdf1a9abeebe32
CurtisNewbie <zhuangyongj@gmail.com>
2020-06-22 20:58:52 +0800

    UnionQuickUnion
    - Worst case O(n) union(), find(), connected()
    - Worst case scenorio, for n elements, union() is O(n^2) time

---
+++ chap01/UnionQuickUnion.java
@@ -0,0 +1,52 @@
+/**
+ * UnionQuick implementation that treats the int[] array as a way to traverse
+ * back to each element's root. I.e., a tree is created for each element
+ */
+public class UnionQuickUnion implements UnionFind {
+
+    private int[] root;
+    private int count;
+
+    public UnionQuickUnion(final int numOfElements) {
+        this.root = new int[numOfElements];
+        this.count = numOfElements;
+    }
+
+    // O(1)
+    @Override
+    public int count() {
+        return count;
+    }
+
+    // worst case: O(n)
+    @Override
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    // worst case: O(n)
+    @Override
+    public int find(int p) {
+        // find root, the root of p will be the one that points to itself,
+        // i.e., root[p] -> p, or, root[3] == 3
+        while (root[p] != p)
+            p = root[p];
+        return root[p];
+    }
+
+    // worst case: O(n)
+    @Override
+    public void union(int p, int q) {
+        if (p == q) {
+            // we can't connect same nodes
+            return;
+        }
+        int rootP = find(p);
+        int rootQ = find(q);
+        // connect root of p to the tree rooted at q's root
+        // i.e., connect these two sub-trees
+        if (rootP != rootQ)
+            root[rootP] = rootQ;
+        count--;
+    }
+}
