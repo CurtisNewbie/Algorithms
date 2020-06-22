5cf21ac02a885e26f53caeef0e225842a880fa14
CurtisNewbie <zhuangyongj@gmail.com>
2020-06-22 23:47:29 +0800

    InsertionSort

---
+++ chap02/InsertionSort.java
@@ -0,0 +1,17 @@
+public class InsertionSort extends AbstractSort {
+
+    public static void main(String[] args) {
+        AbstractSort.main(new InsertionSort());
+    }
+
+    @Override
+    void sort(Comparable[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int j = i + 1;
+            while (j > 0 && lessThan(arr[j], arr[j - 1])) {
+                swap(arr, j, j - 1);
+                j--;
+            }
+        }
+    }
+}
