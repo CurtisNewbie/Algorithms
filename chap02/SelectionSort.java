60202a0ed2612a544f7fee6b97832c2224af2205
CurtisNewbie <zhuangyongj@gmail.com>
2020-06-22 23:26:34 +0800

    SelectionSort

---
+++ chap02/SelectionSort.java
@@ -0,0 +1,21 @@
+public class SelectionSort extends AbstractSort {
+
+    public static void main(String[] args) {
+        AbstractSort.main(new SelectionSort());
+    }
+
+    @Override
+    void sort(Comparable[] arr) {
+        if (arr.length == 0)
+            return;
+        int min;
+        for (int j = 0; j < arr.length; j++) {
+            min = j;
+            for (int i = j + 1; i < arr.length; i++) {
+                if (lessThan(arr[i], arr[min]))
+                    min = i;
+            }
+            swap(arr, min, j);
+        }
+    }
+}
