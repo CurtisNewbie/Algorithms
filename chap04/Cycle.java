f1f6a786cb8599df99a16c59ebfcb35dd4e6021c
CurtisNewbie <zhuangyongj@gmail.com>
2020-07-18 23:54:44 +0800

    Cycle detection

---
+++ chap04/Cycle.java
@@ -0,0 +1,40 @@
+import java.nio.file.Paths;
+
+public class Cycle {
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public Cycle(Graph g) {
+        marked = new boolean[g.vertices()];
+        hasCycle = false;
+        for (int x = 0; x < g.vertices(); x++) {
+            if (!marked[x]) {
+                dfs(g, x, x);
+            }
+        }
+    }
+
+    private void dfs(Graph g, int v, int source) {
+        marked[v] = true;
+        for (int w : g.adjacent(v)) {
+            if (!marked[w]) {
+                dfs(g, w, v);
+            } else if (w == source) {
+                hasCycle = true;
+            }
+        }
+    }
+
+    public boolean hasCycle() {
+        return hasCycle;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Graph: notConnectedG.txt");
+        Graph g1 = new Graph(Paths.get("notConnectedG.txt"));
+        System.out.printf("Graph: %s", g1.toString());
+        System.out.printf("Graph is connected: %b\n", new DFS(g1, 0).count() == g1.vertices());
+        Cycle c = new Cycle(g1);
+        System.out.printf("Graph has cycle: %b\n", c.hasCycle());
+    }
+}
