5cf73f71d25fb59c324eaa167eed534bfb56871e
CurtisNewbie <zhuangyongj@gmail.com>
2020-07-19 21:30:59 +0800

    Topological Order

---
+++ chap04/Topological.java
@@ -0,0 +1,21 @@
+import java.util.Queue;
+
+public class Topological {
+    private Iterable<Integer> topologicalOrder = null;
+
+    public Topological(Digraph g) {
+        // Topological Order is a Reverse Post-Order of a DAG
+        // thus, there must be no cycle in the graph
+        if (!new DirectedCycle(g).hasCycle()) {
+            topologicalOrder = new DepthFirstOrder(g).reversePostOrder();
+        }
+    }
+
+    public boolean isTopological() {
+        return topologicalOrder != null;
+    }
+
+    public Iterable<Integer> order() {
+        return topologicalOrder;
+    }
+}
